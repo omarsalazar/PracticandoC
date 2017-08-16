740871c28eaedfcf3e3fe7f09b7da92d45a156f3
Omar Salazar Paz <salazarpazomar@gmail.com>
2017-08-15 19:44:03 -0500

    Mi primer programa hecho en C
    No tengo ni idea de como funciona pero estos son los dos archivos que salieron. (Ya sé cómo funciona '<')

---
+++ kk.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main(void){
+	int a, b, c;
+	
+	printf("Ingresa tu edad 7u7 ");
+	scanf("%i",&a);
+	printf("ingresa un numero asi por que si ");
+	scanf("%i",&b);
+	
+	c = a+b;
+	printf("Este es el resultado de sumar esos numeros: " "%i",c);
+}
