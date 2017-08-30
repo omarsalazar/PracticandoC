effb8eb27601121097a14ce18cac4aed84ab6e1f
Omar Salazar Paz <salazarpazomar@gmail.com>
2017-08-29 19:49:13 -0500

    Identado.
    Identé esto.

--- llollos.c
+++ llollos.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 main(){
-int a,b,c;
-printf("Introduce 2 enteros separados por coma: \n");
-scanf("%d,%d,%d", &a,&b,&c);
-printf("Antes del swap a vale \"%d\", b vale \"%d\" y c vale \"%d\"", a,b,c);
-c=a;
-a=b;
-b=c;
-printf("\nDespues de swap a vale \"%d\" y b vale \"%d\" y c vale \"%d\"", a,b,c);
+  int a,b,c;
+  printf("Introduce 2 enteros separados por coma: \n");
+  scanf("%d,%d,%d", &a,&b,&c);
+  printf("Antes del swap a vale \"%d\", b vale \"%d\" y c vale \"%d\"", a,b,c);
+  c=a;
+  a=b;
+  b=c;
+  printf("\nDespues de swap a vale \"%d\" y b vale \"%d\" y c vale \"%d\"", a,b,c);
 }
